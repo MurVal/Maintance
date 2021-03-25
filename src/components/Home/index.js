f6557706d5681f9d528507b6af269a972198ddb0
Ablax <ablax@ablax.me>
2021-03-25 04:12:03 +0200

    Use external url for tasks

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,14 +8,19 @@ import MainBody from "./MainBody";
 function Home() {
   const [header, setHeader] = useState(null);
   const [body, setBody] = useState(null);
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    axios.get(process.env.REACT_API_URL).then(({ data }) => {
+    axios.get(`${process.env.REACT_API_URL}/api/config/`).then(({ data }) => {
       const { title, description } = data;
       setHeader(title ? title : "Профилактика");
       setBody(
         description ? description : "В момента се извършва профилактика!"
       );
+    });
+
+    axios.get(`${process.env.REACT_API_URL}/api/task/`).then(({ data }) => {
+      setTasks(data);
     });
   }, []);
 
